3c6416603d22d22cefbc79b97257b656f5999804
Muhammad Ismail Soboute <misoboute@gmail.com>
2018-08-21 14:01:44 +0430

    VTK HelloWorld sample with a QVTKOpenGLWidget; Windows OK

--- main.cpp
+++ main.cpp
@@ -1,6 +1,88 @@
 #include <iostream>
 
+#include <QVTKOpenGLWidget.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCylinderSource.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderer.h>
+
+#include <QApplication>
+#include <QDebug>
+
+#include "SMDebug.h"
+
+vtkSmartPointer<vtkRenderWindow> AddProps()
+{
+    // This creates a polygonal cylinder model with eight circumferential facets
+    // (i.e, in practice an octagonal prism).
+    vtkSmartPointer<vtkCylinderSource> cylinder =
+        vtkSmartPointer<vtkCylinderSource>::New();
+    cylinder->SetResolution(8);
+
+    // The mapper is responsible for pushing the geometry into the graphics library.
+    // It may also do color mapping, if scalars or other attributes are defined.
+    vtkSmartPointer<vtkPolyDataMapper> cylinderMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+
+    // The actor is a grouping mechanism: besides the geometry (mapper), it
+    // also has a property, transformation matrix, and/or texture map.
+    // Here we set its color and rotate it around the X and Y axes.
+    vtkSmartPointer<vtkActor> cylinderActor =
+        vtkSmartPointer<vtkActor>::New();
+    cylinderActor->SetMapper(cylinderMapper);
+    cylinderActor->GetProperty()->SetColor(1.0000, 0.3882, 0.2784);
+    cylinderActor->RotateX(30.0);
+    cylinderActor->RotateY(-45.0);
+
+    // The renderer generates the image
+    // which is then displayed on the render window.
+    // It can be thought of as a scene to which the actor is added
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(cylinderActor);
+    renderer->SetBackground(0.1, 0.2, 0.4);
+    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Zoom(1.5);
+
+    // The render window is the actual GUI window
+    // that appears on the computer screen
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    renderWindow->SetSize(200, 200);
+    renderWindow->AddRenderer(renderer);
+    
+    return renderWindow;
+
+    // The render window interactor captures mouse events
+    // and will perform appropriate camera or actor manipulation
+    // depending on the nature of the events.
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    // This starts the event loop and as a side effect causes an initial render.
+    renderWindowInteractor->Start();
+}
+
 int main(int argc, char** argv)
 {
-    return 0;
+    // This must be done before instantiating the app in order to setup the
+    // right context for our widget.
+    QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+    QApplication app(argc, argv);
+    QScopedPointer<QVTKOpenGLWidget> qvtkWidget(new QVTKOpenGLWidget);
+
+    auto renderWindow = AddProps();
+    qvtkWidget->SetRenderWindow(renderWindow);
+
+    // You can continue to use `window` as a regular vtkRenderWindow
+    // including adding renderers, actors etc.
+    // Show the widget and start the app.
+    qvtkWidget->show();
+    return app.exec();
 }
