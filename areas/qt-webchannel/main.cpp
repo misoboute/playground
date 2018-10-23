27dd22171229be802ba42e7947522bd3c073b14a
Muhammad Ismail Soboute <misoboute@gmail.com>
2018-10-23 14:53:30 +0330

    webchannel load a simple URL into the webengineview widget

--- areas/qt-webchannel/main.cpp
+++ areas/qt-webchannel/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+#include <QApplication>
+#include <QBoxLayout>
+#include <QWebEngineView>
+#include <QWidget>
+
 #include "SMDebug.h"
 
 int main(int argc, char ** argv)
 {
+    QApplication app(argc, argv);
+    QWidget mainWidget;
+    auto mainWidgetLayout = new QVBoxLayout(&mainWidget);
+    mainWidget.setLayout(mainWidgetLayout);
+    auto webView = new QWebEngineView(&mainWidget);
+    webView->setUrl(QUrl("https://www.google.com"));
+    mainWidgetLayout->addWidget(webView, 1);
+    mainWidget.showMaximized();
     SMDBG;
+    return app.exec();
 }
