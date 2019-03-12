e457a4e32de5102de16733027139170dffe354dd
Muhammad Ismail Soboute <misoboute@MacBook-Pro.local>
2019-03-12 21:26:00 +0330

    Demonstrate passing smart ptrs into object ctors

--- areas/MoveCopyCtors/main.cpp
+++ areas/MoveCopyCtors/main.cpp
@@ -2,78 +2,249 @@
 #include <string>
 #include <vector>
 
-class Obj
+namespace my 
+{
+
+template <typename T>
+class shared_ptr
+{
+public:
+    shared_ptr(T* ptr)
+    : ctl_(new ctl_block_t(ptr))
+    {
+        std::cout << "Managing object " << *ctl_->ptr_ << ", ref = " 
+            << ctl_->ref_ << std::endl;
+    }
+    
+    shared_ptr(const my::shared_ptr<T>& other)
+    : ctl_(other.ctl_)
+    {
+        ++ctl_->ref_;
+        std::cout << "New ref to object " << *ctl_->ptr_ << ", ref = " 
+            << ctl_->ref_ << std::endl;
+    }
+    
+    shared_ptr(my::shared_ptr<T>&& other)
+    : ctl_(other.ctl_)
+    {
+        std::cout << "Moving ref to object " << *ctl_->ptr_ << ", ref = " 
+            << ctl_->ref_ << std::endl;
+        other.ctl_ = nullptr;
+    }
+    
+    ~shared_ptr()
+    {
+        if (ctl_ == nullptr)
+        {
+            std::cout << "Destroying an empty ref" << std::endl;
+            return;
+        }
+        std::cout << "Destroying last ref to object " << *ctl_->ptr_ << 
+            ", ref = " << ctl_->ref_ << std::endl;
+        if (--ctl_->ref_ == 0)
+        {
+            std::cout << "Destroying object " << *ctl_->ptr_ << std::endl;
+            delete ctl_;
+        }
+    }
+
+private:    
+    struct ctl_block
+    {
+        ctl_block(T* ptr)
+        : ptr_(ptr)
+        {
+        }
+
+        ~ctl_block()
+        {
+            if (ptr_ != nullptr)
+            {
+                delete ptr_;
+            }
+        }
+
+        T * ptr_;
+        size_t ref_ = 1;
+    };
+
+    using ctl_block_t = typename shared_ptr<T>::ctl_block;
+
+    ctl_block_t * ctl_;
+};
+
+template <typename T>
+class unique_ptr
 {
 public: 
-    Obj(int m)
-    : m_(m)
+    unique_ptr(T* ptr)
+    : ptr_(ptr)
     {
-        std::cout << "Obj default " << m_ << std::endl;
+        std::cout << "Managing object " << *ptr_ << std::endl;
     }
     
-    Obj(const Obj& other)
-    : m_(other.m_)
+    unique_ptr(unique_ptr<T>&& other)
+    : ptr_(other.ptr_)
     {
-        std::cout << "Obj copy " << m_ << std::endl;
+        std::cout << "Moving ref to object " << *ptr_ << std::endl;
+        other.ptr_ = nullptr;
     }
     
-    Obj(Obj&& other)
-    : m_(other.m_)
+    ~unique_ptr()
     {
-        other.m_ = 0;
-        std::cout << "Obj move " << m_ << std::endl;
+        if (ptr_ == nullptr)
+        {
+            std::cout << "Destroying an empty unique_ptr" << std::endl;
+            return;
+        }
+        std::cout << "Destroying object " << *ptr_ << std::endl;
+        delete ptr_;
     }
     
-    ~Obj()
-    {
-        std::cout << "Obj dtor " << m_ << std::endl;
-    }
-    
-    int m_;
+private:
+    T * ptr_;
 };
+
+}
+
+using int_sptr = my::shared_ptr<int>;
+using int_uptr = my::unique_ptr<int>;
 
 class A
 {
 public:
-    A(Obj o)
-    : o_(std::move(o)) {}
+    A(int_sptr o)
+    : o_(std::move(o)) 
+    {
+        std::cout << "A: Pass shared_ptr by value then move" << std::endl;
+    }
 
 private:
-    Obj o_;
+    int_sptr o_;
 };
 
 class B
 {
 public:
-    B(const Obj& o)
-    : o_(o) {}
+    B(const int_sptr& o)
+    : o_(o) 
+    {
+        std::cout << 
+            "B: Pass shared_ptr by const ref and don't move" << std::endl;
+    }
 
 private:
-    Obj o_;
+    int_sptr o_;
 };
 
 class C
 {
 public:
-    C(Obj&& o)
-    : o_(std::move(o)) {}
-
+    C(int_sptr&& o)
+    : o_(std::move(o))
+    {
+        std::cout << "C: Pass shared_ptr by rvalue ref then move" << std::endl;
+    }
+    
 private:
-    Obj o_;
+    int_sptr o_;
+};
+
+class D
+{
+public:
+    D(const int_sptr& o)
+    : o_(std::move(o))
+    {
+        std::cout << "D: Pass shared_ptr by const ref then move" << std::endl;
+    }
+    
+private:
+    int_sptr o_;
+};
+
+class E
+{
+public:
+    E(int_uptr o)
+    : o_(std::move(o))
+    {
+        std::cout << "E: Pass unique_ptr by value then move" << std::endl;
+    }
+    
+private:
+    int_uptr o_;
+};
+
+class F
+{
+public:
+    F(int_uptr&& o)
+    : o_(std::move(o))
+    {
+        std::cout << "F: Pass unique_ptr by value then move" << std::endl;
+    }
+    
+private:
+    int_uptr o_;
 };
 
 int main()
 {
-    Obj oA(1), oB(2), oC(3), oA4(4), oB5(5);
-    std::cout << "Creating A" << std::endl;
-    A a(oA);
-    std::cout << "Creating A 4" << std::endl;
-    A b(std::move(oA4));
-    std::cout << std::endl << "Creating B" << std::endl;
-    B c(oB);
-    std::cout << std::endl << "Creating B 5" << std::endl;
-    B d(std::move(oB5));
-    std::cout << std::endl << "Creating C" << std::endl;
-    C e(std::move(oC));
-    std::cout << std::endl << "Done" << std::endl;
+    {
+        std::cout << std::endl << "Creating A with lvalue 1" << std::endl;
+        int_sptr s(new int(1));
+        A a(s);
+    }
+
+    {
+        std::cout << std::endl << "Creating A with rvalue 1" << std::endl;
+        A a(int_sptr(new int(1)));
+    }
+
+    {
+        std::cout << std::endl << "Creating B with lvalue 2" << std::endl;
+        int_sptr s(new int(2));
+        B b(s);
+    }
+
+    {
+        std::cout << std::endl << "Creating B with rvalue 2" << std::endl;
+        B b(int_sptr(new int(2)));
+    }
+
+    std::cout << std::endl << 
+        "Creating C with lvalue is not possible" << std::endl;
+    
+    {
+        std::cout << std::endl << "Creating C with rvalue 3" << std::endl;
+        C c(int_sptr(new int(3)));
+    }
+
+    {
+        std::cout << std::endl << "Creating D with lvalue 4" << std::endl;
+        int_sptr s(new int(4));
+        D d(s);
+    }
+
+    {
+        std::cout << std::endl << "Creating D with rvalue 4" << std::endl;
+        D d(int_sptr(new int(4)));
+    }
+
+    std::cout << std::endl << 
+        "Creating E with lvalue is not possible" << std::endl;
+
+    {
+        std::cout << std::endl << "Creating E with rvalue 5" << std::endl;
+        E e(int_uptr(new int(5)));
+    }
+
+    std::cout << std::endl << 
+        "Creating F with lvalue is not possible" << std::endl;
+
+    {
+        std::cout << std::endl << "Creating F with rvalue 6" << std::endl;
+        F f(int_uptr(new int(6)));
+    }
 }
